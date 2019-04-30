be2aa04d75c12c6f3badc6c8d6c0823f7e903f70
Sushant Ubale <sushantubale@gmail.com>
2019-04-30 16:50:47 -0500

    Variables and Constants

---
+++ VariablesAndConstants.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Variable
+
+var someInteger: Int = 10
+var someInteger1 = 10
+someInteger = 20
+var someString: String = "someString"
+
+
+//Constant
+
+let someIntegerConstant = 20.0
+
+// Type Inference
+// Swift is smart enough to detect the type of any constant or variable.
+
+var someImportantValue = "some important value"
