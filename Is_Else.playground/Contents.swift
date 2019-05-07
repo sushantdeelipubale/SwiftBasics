801002737ddaaca5ec100380f45fc17008e5c413
Sushant Ubale <sushantubale@gmail.com>
2019-05-07 15:32:19 -0500

    if else

---
+++ Is_Else.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var someInteger = 10
+if someInteger > 5 {
+    //print("Yalalllalallallaalal, This number is greater that 5")
+}
+
+var string = "Some String"
+if string == "drddrgddf" {
+//    print("ananannaannaanan, This is not Some String")
+} else {
+    //print("This is some string")
+}
+
+var someIntegerValue = 24
+
+if someIntegerValue > 10 {
+    print("This number is greater than 100")
+} else if someIntegerValue > 20 {
+    print("This number is greater than 50")
+} else {
+    print("this number shd")
+}
+print("Future functionality")
